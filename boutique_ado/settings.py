6c6fe01d6855fbce67b3229ec7bf59c8e90663f8
Chris Curran <chriscurran8@yahoo.com>
2021-01-13 12:40:18 +0000

    added allauth templates and base template

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth', # required by allauth - DON'T remove!
+                'django.contrib.auth.context_processors.auth',  # required by allauth - DON'T remove!
                 'django.contrib.messages.context_processors.messages',
             ],
         },

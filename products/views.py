b19d107c4e80f87ed7b6f417263417de56c98ca8
Chris Curran <chriscurran8@yahoo.com>
2021-01-20 06:10:24 +0000

    added JS sorting and back to top button

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
+from django.db.models.functions import Lower
 
 
 def all_products(request):

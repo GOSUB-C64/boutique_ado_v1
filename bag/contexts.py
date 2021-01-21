85417718f5ea357de96b3e597e5fd33aee3f448e
Chris Curran <chriscurran8@yahoo.com>
2021-01-21 05:41:16 +0000

    added add to bag functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -13,7 +13,7 @@ def bag_contents(request):
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
-        free_delivery = 0
+        free_delivery_delta = 0
 
     grand_total = delivery + total
 
--- products/views.py
+++ products/views.py
@@ -1,9 +1,9 @@
-
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product, Category
 from django.db.models.functions import Lower
+
+from .models import Product, Category
 
 
 def all_products(request):
@@ -22,25 +22,25 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-    if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse, ('products'))
-
+                return redirect(reverse('products'))
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 

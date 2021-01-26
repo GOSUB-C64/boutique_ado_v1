450c075d15212fef61b1a8a23d49cf745441968a
Chris Curran <chriscurran8@yahoo.com>
2021-01-26 08:32:38 +0000

    created checkout app and models

--- bag/views.py
+++ bag/views.py
@@ -77,7 +77,6 @@ def adjust_bag(request, item_id):
             bag.pop(item_id)
             messages.success(request, f'Removed {product.name} from your bag')
 
-
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 

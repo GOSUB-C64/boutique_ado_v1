866de099a54b3793f880c9f7ed893b3a328550ef
Chris Curran <chriscurran8@yahoo.com>
2021-02-21 07:16:50 +0000

    Product Admin - finished add product functionality

--- profiles/views.py
+++ profiles/views.py
@@ -15,8 +15,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
@@ -44,4 +46,3 @@ def order_history(request, order_number):
     }
 
     return render(request, template, context)
-    
